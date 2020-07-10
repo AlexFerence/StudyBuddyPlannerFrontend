247c958abbc5d27e52ebc8f7562e747a54581dee
Alex Ference <alexference23@gmail.com>
2020-07-09 23:04:54 -0700

    First dashboard timer implemented

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const Dashboard = () => {
-    return (
-        <div>Dashboard page</div>
-    )
+const minuteSeconds = 60;
+
+const timerProps = {
+  isPlaying: true,
+  size: 300,
+  strokeWidth: 6,
+  duration: 0,
+  
+
+};
+
+export default function App() {
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + 243240; // use UNIX timestamp in seconds
+
+  const remainingTime = endTime - stratTime;
+
+  return (
+    <div className="App">
+      <CountdownCircleTimer
+        {...timerProps}
+        colors={[["#218380"]]}
+        duration={minuteSeconds}
+        colors={[[ '#FB275D' ]]}
+        
+
+        initialRemainingTime={ 60- (remainingTime % minuteSeconds)}
+        onComplete={() => {
+            //totalElapsedTime => [remainingTime - totalElapsedTime > 0]
+            console.log('finished')
+
+        } }
+      >
+        {
+            ({ elapsedTime }) => <h3>{parseInt(elapsedTime)}</h3>
+        }
+      </CountdownCircleTimer>
+    </div>
+  );
 }
-
-export default Dashboard
