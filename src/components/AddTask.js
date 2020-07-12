7be567e8329dfe39f1eb4d1d65a7c770c2e7b8b2
Alex Ference <alexference23@gmail.com>
2020-07-12 15:00:09 -0700

    add datepicker next

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
+import url from '../environment/url'
 
 const TaskModal = ({ subjects, turnOffAdding }) => {
 
@@ -11,18 +13,26 @@ const TaskModal = ({ subjects, turnOffAdding }) => {
     useEffect(() => {
         if (subjects[0]) {
             setCurrentSubjectID(subjects[0].id)
-            setTaskType(subjects[0].taskTitle)
         }
 
-    })
+    },[])
 
     const onSubmit = (e) => {
         e.preventDefault()
-
         console.log(currentSubjectID)
-        console.log(taskTitle)
         console.log(taskType)
+        console.log(taskTitle)
+        console.log(taskDesc)
+        axios.post(url + "/api/Tasks/create", {
+            taskType,
+            description: taskDesc,
+            hours: 0,
+            subjectID: currentSubjectID,
 
+        })
+
+        // TODO
+        // ADD DUE DATE
     }
 
     return (
@@ -38,7 +48,8 @@ const TaskModal = ({ subjects, turnOffAdding }) => {
                     <div className="top-row">
                     </div>
                 Class:
-                <select onChange={(e) => setCurrentSubjectID(e.target.value) }>
+                <select required onChange={(e) => setCurrentSubjectID(e.target.value) }>
+                    <option>Choose Class</option>
                         {
                             subjects.map((subject) => {
                                 return (<option value={subject.id} key={subject.id}>{subject.name} {subject.classCode}</option>)
@@ -46,15 +57,22 @@ const TaskModal = ({ subjects, turnOffAdding }) => {
                         }
                     </select>
                 Type:
-                <select onChange={(e) => setTaskType(e.target.value)}>
+                    <select name="choose type" required onChange={(e) => {
+                        console.log(e.target.value)
+                        setTaskType(e.target.value)
+                    }}>
+                        
                         <option value="Assignment">Assignment</option>
                         <option value="Quiz">Quiz</option>
                         <option value="Test">Test</option>
                         <option value="Exam">Exam</option>
                     </select> <br />
 
+
                     <label>Title: </label><input value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} />
-                    <label>Description: </label><textarea />
+                    <label>Description: </label><textarea onChange={(e) => {
+                        setTaskDesc(e.target.value)
+                    }} />
                     <button type="submit">Submit</button>
                     <button onClick={(e) => {
                         e.preventDefault()
