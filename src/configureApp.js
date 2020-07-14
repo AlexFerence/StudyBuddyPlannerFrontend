52f1690233539165c8ef5592db8cd25b2313dd86
Alex Ference <alexference23@gmail.com>
2020-07-14 11:00:54 -0700

    more debugging

--- src/configureApp.js
+++ src/configureApp.js
@@ -5,7 +5,6 @@ import LoginPage from './components/LoginPage'
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header'
 import Dashboard from './components/Dashboard'
-import Calendar from './components/Calendar'
 import Tasks from './components/Tasks'
 import Analytics from './components/Analytics'
 import Settings from './components/Settings'
@@ -20,7 +19,6 @@ const ConfigureApp = () => {
         <Route path='/signup' component={FormPage} exact />
         <Route path='/login' exact component={LoginPage} />
         <Route path='/dashboard' component={Dashboard} />
-        <Route path='/calendar' component={Calendar} />
         <Route path='/analytics' component={Analytics} />
         <Route path='/tasks' component={Tasks} />
         <Route path='/settings' component={Settings} />
