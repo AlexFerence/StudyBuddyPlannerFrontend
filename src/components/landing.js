10ce8fb11621c933842db538b3222106b2d6fedc
Alex Ference <alexference23@gmail.com>
2020-07-26 09:21:02 -0700

    landing should be fixed

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 const Landing = () => {
     return (
         <div>
-            hello
             <Link to="/signup">Sign Up</Link>
             <Link to="/login">Log In</Link>
         </div>
