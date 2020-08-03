e96fa8d4bb4acf8d6f7cd85de0beaa59ab542d3a
Alex Ference <alexference23@gmail.com>
2020-08-03 14:18:14 -0700

    landing page main section almost done

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,12 +4,11 @@ import { Nav, Navbar, Button, Form, FormControl, Row, Col } from 'react-bootstra
 import { connect } from 'react-redux'
 import laptop from '../assets/laptop.jpg'
 import pencils from '../assets/pencils.jpg'
+import ReactEcharts from 'echarts-for-react'
 
 const Landing = ({ history, profile }) => {
     useEffect(() => {
-        // if (profile.email) {
-        //     history.push('dashboard')
-        // }
+       
 
     }, [])
 
@@ -39,25 +38,109 @@ const Landing = ({ history, profile }) => {
                 onClick={goToSignUp}>Sign Up</button></div>
                 </div>
             </div>
-            <div className="about">
+            <div className="rowSection">
             <Row style={{ height: '100%' }}>
-            <Col className="aboutText">
-            <div className="aboutInner">
-                <div className="aboutHeader">About Us</div>
-                <div className="aboutPara">    omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
+            <Col md={6} className="textCol">
+            <div className="textInner">
+                <div className="textHeader">About Us</div>
+                <div className="textPara">    omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
                 ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
-                Spopularizovaný bol v <br />
+                Spopularizovaný bol v</div><div className="textPara">
        
                 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali 
                 pasáže Loreorem Ipsum.</div>
             </div>
             </Col>
-            <Col className="aboutDisplay">
-                <img src={laptop} className="laptopImage"/>
+            <Col className="imageCol" >
+                <img src={laptop} className="imageInner"/>
             </Col>
             </Row>
             </div>
 
+            <div className="rowSection">
+            <Row style={{ height: '100%' }}>
+            <Col md={6} className="imageCol">
+            <div className="imageInner">
+            <ReactEcharts
+            option={{
+              series: [
+                {
+                  type: 'pie',
+                  radius: '65%',
+                  center: ['50%', '50%'],
+                  selectedMode: 'single',
+                  data:[
+                    {name: 'MATH', value: 66},
+                    {name: 'PHYS', value: 33},
+                    {name: 'BIO', value: 44},
+                    {name: 'ENGL', value: 30},
+                    {name: 'HIST', value: 10},
+                  ]
+                  ,
+                  emphasis: {
+                    itemStyle: {
+                      shadowBlur: 10,
+                      shadowOffsetX: 0,
+                      shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                  }
+                }
+              ]
+
+            }}
+          />
+            </div>
+            </Col>
+            <Col md={6} className="textCol">
+            <div className="textInner">
+                <div className="textHeader">Features</div>
+                <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
+                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
+                Spopularizovaný bol v</div><div className="textPara">
+       
+                60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali 
+                pasáže Loreorem Ipsum.</div>
+            </div>
+            </Col>
+            </Row>
+            </div>
+            <div className="rowSection">
+            <Row style={{ height: '100%' }}>
+            <Col md={6} className="textCol">
+            <div className="textInner">
+                <div className="textHeader">In-Depth Data Analysis</div>
+                <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
+                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
+                Spopularizovaný bol v</div>
+            </div>
+            </Col>
+            <Col md={6} className="imageCol">
+            <div className="imageInner">
+            <ReactEcharts
+              option={{
+                xAxis: {
+                  type: 'category',
+                  data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                },
+                yAxis: {
+                  type: 'value',
+                  axisLabel : {
+                    formatter: '{value}'
+                  },
+                  name: 'hours',
+                  nameLocation: 'middle',
+                  nameGap: 35
+                },
+                series: [{
+                  data: [4,5,3.5,5.5,2, 3,6],
+                  type: 'bar'
+                }]
+              }}
+            />
+            </div>
+            </Col>
+            </Row>
+            </div>
 
             <Link to="/signup">Sign Up</Link>
             <Link to="/login">Log In</Link>
