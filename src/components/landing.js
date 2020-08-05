33e66c3d73f9c56fb6253d45b543828f1e879c1e
Alex Ference <alexference23@gmail.com>
2020-08-05 16:41:16 -0700

    landing done

--- src/components/landing.js
+++ src/components/landing.js
@@ -9,228 +9,220 @@ import ReactPlayer from 'react-player'
 
 
 const Landing = ({ history, profile }) => {
-    useEffect(() => {
-       
+  useEffect(() => {
 
-    }, [])
 
-    const goToSignUp = () => {
-        history.push("/signup")
-    }
+  }, [])
 
-    return (
-        <div className="landing">
-            <div className="landingNav">
-                <button id="brand" className="navButton">Peak</button>
-                <div>
-                    <button className="navButton">About</button>
-                    <button className="navButton">Features</button>
-                    <button className="navButton">Premium</button>
-                    <button className="navButtonSignUp"
-                    onClick={goToSignUp}
-                    >Sign Up</button>
-                </div>
-            </div>
+  const goToSignUp = () => {
+    history.push("/signup")
+  }
 
-        <div className="landingDisplay" style={{ backgroundColor: 'red' }}>
-          <video id="background-video" loop autoPlay>
-            <source src="/videos/video.mp4" type="video/mp4" />
-            <source src="/videos/video.mp4" type="video/ogg" />
+  return (
+    <div className="landing">
+      <div className="landingNav">
+        <button id="brand" className="navButton">Peak</button>
+        <div>
+          <button className="navButton">About</button>
+          <button className="navButton">Features</button>
+          <button className="navButton">Premium</button>
+          <button className="navButtonSignUp"
+            onClick={goToSignUp}
+          >Sign Up</button>
+        </div>
+      </div>
+
+      <div className="landingDisplay">
+        <video id="background-video" loop autoPlay>
+          <source src="/videos/video.mp4" type="video/mp4" />
+          <source src="/videos/video.mp4" type="video/ogg" />
             Your browser does not support the video tag.
             </video>
-            <div class="overlay">
-        <p>Content above your video</p>
-        <form>
-            <p>Content Below Your Video</p>
-            <label for="input">Form Input Label</label>
-            <input id="input" name="input" value="" />
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-            
-            </div>
+      </div>
+      <div class="overlay landingDisplay">
+          <div className="center">
+            <div className="promotion">Stay on top of your studies.</div>
+            <div className="promotion">Stay on top of your life.</div>
+            <div><button id="mainButton"
+              className="navButtonSignUp"
+              onClick={goToSignUp}>Sign Up</button></div>
 
-        <div className='player-wrapper'>
+          </div>
+        </div>
 
-            <div className="landingDisplay">
-                <div>
-                <div className="promotion">Stay on top of your studies.</div>
-                <div className="promotion">Stay on top of your life.</div>
-                <div><button id="mainButton" 
-                className="navButtonSignUp"
-                onClick={goToSignUp}>Sign Up</button></div>
-                </div>
-            </div>
-            <div className="rowSection">
-            <Row style={{ height: '100%' }}>
+      <div className='player-wrapper'>
+
+        
+        <div className="rowSection">
+          <Row style={{ height: '100%' }}>
             <Col md={6} className="textCol">
-            <div className="textInner">
+              <div className="textInner">
                 <div className="textHeader">About Us</div>
-                <div className="textPara">    omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
-                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
+                <div className="textPara">    omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí,
+                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený.
                 Spopularizovaný bol v</div><div className="textPara">
-       
-                60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali 
+
+                  60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali
                 pasáže Loreorem Ipsum.</div>
-            </div>
+              </div>
             </Col>
             <Col className="imageCol" >
-                <img src={laptop} className="imageInner"/>
+              <img src={laptop} className="imageInner" />
             </Col>
-            </Row>
-            </div>
+          </Row>
+        </div>
 
-            <div className="rowSection">
-            <Row style={{ height: '100%' }}>
+        <div className="rowSection">
+          <Row style={{ height: '100%' }}>
             <Col md={6} className="imageCol">
-            <div className="imageInner">
-            <ReactEcharts
-            option={{
-              series: [
-                {
-                  type: 'pie',
-                  radius: '65%',
-                  center: ['50%', '50%'],
-                  selectedMode: 'single',
-                  data:[
-                    {name: 'MATH', value: 66},
-                    {name: 'PHYS', value: 33},
-                    {name: 'BIO', value: 44},
-                    {name: 'ENGL', value: 30},
-                    {name: 'HIST', value: 10},
-                  ]
-                  ,
-                  emphasis: {
-                    itemStyle: {
-                      shadowBlur: 10,
-                      shadowOffsetX: 0,
-                      shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                  }
-                }
-              ]
+              <div className="imageInner">
+                <ReactEcharts
+                  option={{
+                    series: [
+                      {
+                        type: 'pie',
+                        radius: '65%',
+                        center: ['50%', '50%'],
+                        selectedMode: 'single',
+                        data: [
+                          { name: 'MATH', value: 66 },
+                          { name: 'PHYS', value: 33 },
+                          { name: 'BIO', value: 44 },
+                          { name: 'ENGL', value: 30 },
+                          { name: 'HIST', value: 10 },
+                        ]
+                        ,
+                        emphasis: {
+                          itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                          }
+                        }
+                      }
+                    ]
 
-            }}
-          />
-            </div>
+                  }}
+                />
+              </div>
             </Col>
             <Col md={6} className="textCol">
-            <div className="textInner">
+              <div className="textInner">
                 <div className="textHeader">Features</div>
-                <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
-                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
+                <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí,
+                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený.
                 Spopularizovaný bol v</div><div className="textPara">
-       
-                60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali 
+
+                  60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali
                 pasáže Loreorem Ipsum.</div>
-            </div>
+              </div>
             </Col>
-            </Row>
-            </div>
-            <div className="rowSection">
-            <Row style={{ height: '100%' }}>
+          </Row>
+        </div>
+        <div className="rowSection">
+          <Row style={{ height: '100%' }}>
             <Col md={6} className="textCol">
-            <div className="textInner">
+              <div className="textInner">
                 <div className="textHeader">In-Depth Data Analysis</div>
                 <div className="textPara">Peak offers in depth analysis on all of your studies. Let us do the work for you. We will help you
                 track how you divide your time through the week, across all your subjects, and much more.</div>
-            </div>
+              </div>
             </Col>
             <Col md={6} className="imageCol">
-            <div className="imageInner">
-            <ReactEcharts
-              option={{
-                xAxis: {
-                  type: 'category',
-                  data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                  axisLine: {
-                    lineStyle: {
-                      color: '#FFF'
-                    }
-                  }
-                },
-                yAxis: {
-                  type: 'value',
-                  axisLine: {
-                    lineStyle: {
-                      color: '#FFF'
-                    }
-                  },
-                  name: 'hours',
-                  nameLocation: 'middle',
-                  nameGap: 35
-                },
-                series: [{
-                  data: [4,5,3.5,5.5,2, 3,6],
-                  type: 'bar'
-                }]
-              }}
-            />
-            </div>
+              <div className="imageInner">
+                <ReactEcharts
+                  option={{
+                    xAxis: {
+                      type: 'category',
+                      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                      axisLine: {
+                        lineStyle: {
+                          color: '#FFF'
+                        }
+                      }
+                    },
+                    yAxis: {
+                      type: 'value',
+                      axisLine: {
+                        lineStyle: {
+                          color: '#FFF'
+                        }
+                      },
+                      name: 'hours',
+                      nameLocation: 'middle',
+                      nameGap: 35
+                    },
+                    series: [{
+                      data: [4, 5, 3.5, 5.5, 2, 3, 6],
+                      type: 'bar'
+                    }]
+                  }}
+                />
+              </div>
             </Col>
-            </Row>
-            <div className="rowSection">
+          </Row>
+          <div className="rowSection">
             <Row style={{ height: '100%' }}>
-            <Col md={6} className="imageCol">
-            <div className="imageInner">
-            <ReactEcharts
-              option={{
-                xAxis: {
-                  type: 'category',
-                  data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                  axisLine: {
-                    lineStyle: {
-                      color: '#FFF'
-                    }
-                  }
-                },
-                yAxis: {
-                  type: 'value',
-                  axisLine: {
-                    lineStyle: {
-                      color: '#FFF'
-                    }
-                  },
-                  name: 'hours',
-                  nameLocation: 'middle',
-                  nameGap: 35
-                },
-                series: [{
-                  data: [4,5,3.5,5.5,2, 3,6],
-                  type: 'line'
-                },
-                {
-                  color: '#add8e6',
-                  data: [1,4,2,3.5,6,4,3],
-                  type: 'line'
-                }
-              ]
-              }}
-            />
-            </div>
-            </Col>
-            <Col md={6} className="textCol">
-            <div className="textInner">
-                <div className="textHeader">Features</div>
-                <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, 
-                ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. 
+              <Col md={6} className="imageCol">
+                <div className="imageInner">
+                  <ReactEcharts
+                    option={{
+                      xAxis: {
+                        type: 'category',
+                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                        axisLine: {
+                          lineStyle: {
+                            color: '#FFF'
+                          }
+                        }
+                      },
+                      yAxis: {
+                        type: 'value',
+                        axisLine: {
+                          lineStyle: {
+                            color: '#FFF'
+                          }
+                        },
+                        name: 'hours',
+                        nameLocation: 'middle',
+                        nameGap: 35
+                      },
+                      series: [{
+                        data: [4, 5, 3.5, 5.5, 2, 3, 6],
+                        type: 'line'
+                      },
+                      {
+                        color: '#add8e6',
+                        data: [1, 4, 2, 3.5, 6, 4, 3],
+                        type: 'line'
+                      }
+                      ]
+                    }}
+                  />
+                </div>
+              </Col>
+              <Col md={6} className="textCol">
+                <div className="textInner">
+                  <div className="textHeader">Features</div>
+                  <div className="textPara">omiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí,
+                  ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený.
                 Spopularizovaný bol v</div><div className="textPara">
-       
-                60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali 
+
+                    60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali
                 pasáže Loreorem Ipsum.</div>
-            </div>
-            </Col>
+                </div>
+              </Col>
             </Row>
-            </div>
-            </div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 const mapStateToProps = (state) => {
-    return {
-        profile: state.profile
-    }
+  return {
+    profile: state.profile
+  }
 }
 
 export default connect(mapStateToProps)(Landing)
