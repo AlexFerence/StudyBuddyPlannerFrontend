6241b9f7cca5a3f907f3eb8a8b4b62ee9754673e
aferenceKatorio <aference@kator.io>
2020-09-12 22:09:08 -0700

    stopwatch fixed too

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -14,6 +14,7 @@ import {
     loadYearBeakdown, loadFacultyStats, loadAverageOfWeekDay
 } from '../thunks/chartThunk'
 import { refreshAllCharts } from '../thunks/chartThunk'
+import * as workerTimers from 'worker-timers';
 
 const Stopwatch = ({ currentTask, dispatch, id, color, isRunningRedux, paused, setCurrentTask }) => {
     const [count, setCount] = useState(0);
@@ -184,8 +185,8 @@ function useInterval(callback, delay) {
             savedCallback.current();
         }
         if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
+            let id = workerTimers.setInterval(tick, delay);
+            return () => workerTimers.clearInterval(id);
         }
     }, [delay]);
 }
