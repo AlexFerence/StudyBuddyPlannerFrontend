3afb57a339dc2d1aecc611d9adfd82b6ca7e6cdf
alexference <alexference23@gmail.com>
2020-09-22 08:39:32 -0700

    tooltip

--- src/components/friend-components/FriendModalSubjectListItem.js
+++ src/components/friend-components/FriendModalSubjectListItem.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 const FriendModalFriendListItem = ({ subject }) => {
 
 
 
     return (
-        <div className="friend-modal-subject-list-item__class-name">
-            <div style={{ backgroundColor: subject.color }} className="friend-modal-subject-list-item__circle"></div> {subject.name} {subject.classCode}
-        </div>
+        <Tippy content={<div>{subject.description}</div>}
+            placement="left"
+            allowHTML={true}
+            animateFill={false}
+
+        >
+            <div className="friend-modal-subject-list-item__class-name">
+                <div style={{ backgroundColor: subject.color }} className="friend-modal-subject-list-item__circle"></div> {subject.name} {subject.classCode}
+            </div>
+        </Tippy>
+
     )
 }
 
