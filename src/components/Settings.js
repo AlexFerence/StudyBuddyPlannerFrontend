3a66f0fe9ed1469f619b61b71cb7453972f6b677
arikaufman <42683887+arikaufman@users.noreply.github.com>
2020-10-07 11:41:43 -0400

    update settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -177,7 +177,7 @@ const Settings = ({ dispatch, firstName, lastName, emailProp, passwordProp, toke
             >Log Out</button>
             {
                 (emailProp === 'akaufman2000@gmail.com' || emailProp === 'alexference23@gmail.com' ||
-                    emailProp === 'vladstets18@gmail.com' || email === 'Vladstets18@gmail.com') &&
+                    emailProp === 'vladstets18@gmail.com' || email === 'Vladstets18@gmail.com' || emailProp === 'maddy.eppsconn@gmail.com') &&
                 <div>
                     <OverviewBar />
                     <AdminBar />
