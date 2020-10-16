0be9495469bf11e6853ab13aca5e70d3fbafb653
arikaufman <42683887+arikaufman@users.noreply.github.com>
2020-10-15 17:09:32 -0700

    update carousel

--- src/components/landing-carousel.js
+++ src/components/landing-carousel.js
@@ -6,19 +6,23 @@ const LandingCarousel = () => {
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
+            breakpoint: { max: 4000, min: 2500 },
             items: 5
         },
+        largeDesktop: {
+            breakpoint: { max: 2500, min: 1600 },
+            items: 4
+        },
         desktop: {
-            breakpoint: { max: 3000, min: 1024 },
+            breakpoint: { max: 1600, min: 1200 },
             items: 3
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464 },
+            breakpoint: { max: 1200, min: 800 },
             items: 2
         },
         mobile: {
-            breakpoint: { max: 464, min: 0 },
+            breakpoint: { max: 800, min: 0 },
             items: 1
         }
     };
@@ -43,7 +47,7 @@ const LandingCarousel = () => {
                             <InstagramEmbed
                                 key={link}
                                 url={link}
-                                maxWidth={450}
+                                maxWidth={400}
                                 hideCaption={true}
                                 containerTagName='div'
                                 protocol=''
