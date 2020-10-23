468e9a8567a0bb63cb4545e5cd52fd2c779c69f3
aferenceKatorio <aference@kator.io>
2020-10-23 09:56:20 -0700

    landing fix

--- src/configureApp.js
+++ src/configureApp.js
@@ -124,10 +124,7 @@ const ConfigureApp = ({ dispatch, width, isAuth, tokenExpiry, loading }) => {
       {width > 999 && isAuth && <FriendActivity />}
       { false && <Loader />}
       <Switch>
-        <Route path='/' component={() => {
-          window.location.href = 'https://www.studybuddyplanner.com/';
-          return null;
-        }} exact />
+        <Route path='/' component={Landing} exact />
         <Route path='/blog' component={Blog} />
         <Route path='/signup' component={SignUp} />
         <Route path='/signUpSecondary' component={SignUpSecondary} />
@@ -136,7 +133,7 @@ const ConfigureApp = ({ dispatch, width, isAuth, tokenExpiry, loading }) => {
         <Route path='/resetpassword' exact component={ResetPassword} />
         <Route path='/dashboard'>
           {moment().isAfter(moment(tokenExpiry)) ? <Redirect to='/' /> : <Dashboard />}
-        </Route>
+        </Route>ß
         <Route path='/tasks'>
           {moment().isAfter(moment(tokenExpiry)) ? <Redirect to='/' /> : <Tasks />}
         </Route>
