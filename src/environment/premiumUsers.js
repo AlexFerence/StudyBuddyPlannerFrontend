19429b53c308c0e20aa37a1c860ada5541ec7fbc
arikaufman <42683887+arikaufman@users.noreply.github.com>
2020-11-04 19:17:56 -0800

    update

--- src/environment/premiumUsers.js
+++ src/environment/premiumUsers.js
@@ -1,1 +1,6 @@
-export const premiumUsers = ['alexference23@gmail.com', 'akaufman2000@gmail.com']
+export const premiumUsers =
+['alexference23@gmail.com',
+ 'akaufman2000@gmail.com',
+  'maddy.eppsconn@gmail.com',
+  'vladstets18@gmail.com',
+   'Sayeed.mavani@gmail.com']
