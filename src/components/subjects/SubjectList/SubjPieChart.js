247c6bc9ef0fb24b2ffa7e1a5e612c885bc62833
alexference <alexference23@gmail.com>
2020-11-13 21:05:42 -0800

    plz work

--- src/components/subjects/SubjectList/SubjPieChart.js
+++ src/components/subjects/SubjectList/SubjPieChart.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import ReactEcharts from 'echarts-for-react'
 
-const SubjPieChart = ({ breakdownChart, dispatch, currentSubject, data }) => {
+const SubjPieChart = ({ breakdownChart, dispatch, currentSubject, data = [] }) => {
 
     let pieData = []
     data.forEach((item) => {
