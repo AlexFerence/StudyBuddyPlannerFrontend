b7b36c258dc485a77c160af56eef94184a59339e
alexference <alexference23@gmail.com>
2020-12-04 08:26:04 -0800

    Numbers overview fixed

--- src/components/dashboard-components/NumbersOverview.js
+++ src/components/dashboard-components/NumbersOverview.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
+import GraphCoverUp from '../shared/GraphCoverUp'
 
 const NumbersOverView = ({ charts }) => {
 
@@ -11,55 +12,63 @@ const NumbersOverView = ({ charts }) => {
     }
 
     return (
-        <div className="innerBoxCol">
-            <div className="flexNumDisplay" >
-                <div className="rowTitle">Current Stats</div>
-                <div className="row">
-                    <div className="square">
-                        <div className="squareTitle">Today</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentDay)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Past Week</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentWeek)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Past Month</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentMonth)}</div>
+        <Fragment>
+            {charts.personalStats.bestDay ?
+                <div className="innerBoxCol">
+                    <div className="flexNumDisplay" >
+                        <div className="rowTitle">Current Stats</div>
+                        <div className="row">
+                            <div className="square">
+                                <div className="squareTitle">Today</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentDay)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Past Week</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentWeek)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Past Month</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.currentMonth)}</div>
+                            </div>
+                        </div>
+                        <div className="rowTitle">Average Result</div>
+                        <div className="row">
+                            <div className="square">
+                                <div className="squareTitle">Day</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageDay)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Week</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageWeek)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Month</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageMonth)}</div>
+                            </div>
+                        </div>
+                        <div className="rowTitle">Best Result</div>
+                        <div className="row">
+                            <div className="square">
+                                <div className="squareTitle">Day</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestDay)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Week</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestWeek)}</div>
+                            </div>
+                            <div className="square">
+                                <div className="squareTitle">Month</div>
+                                <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestMonth)}</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="rowTitle">Average Result</div>
-                <div className="row">
-                    <div className="square">
-                        <div className="squareTitle">Day</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageDay)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Week</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageWeek)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Month</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.averageMonth)}</div>
-                    </div>
+                :
+                <div className="innerBoxCol">
+                    <GraphCoverUp />
                 </div>
-                <div className="rowTitle">Best Result</div>
-                <div className="row">
-                    <div className="square">
-                        <div className="squareTitle">Day</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestDay)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Week</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestWeek)}</div>
-                    </div>
-                    <div className="square">
-                        <div className="squareTitle">Month</div>
-                        <div className="squareData">{hoursToTimeDisplay(charts.personalStats.bestMonth)}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            }
+        </Fragment>
     )
 }
 
